53819c17fe03ec3cda105d1057693d89e50b45ed
Binsin540 <sdagher540@gmail.com>
2023-06-23 12:35:59 +0300

    add admin PANEL

---
+++ lib/admin_panel/home_page/homePage.dart
@@ -0,0 +1,200 @@
+import 'package:flutter/material.dart';
+
+import '../../ui/style/app_color.dart';
+import '../widgets/multi_item.dart';
+
+class AddFlight extends StatefulWidget {
+  AddFlight({Key? key}) : super(key: key);
+
+  @override
+  State<AddFlight> createState() => _AddFlightState();
+}
+
+class _AddFlightState extends State<AddFlight> {
+  var cityVal;
+  var busnum;
+  List<String> busStop = [];
+  DateTime flightDate = DateTime.now();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: InkWell(
+          child: Container(alignment: Alignment.center,child: Text('Submit',style: TextStyle(color: ColorPattren.darkBlue,fontSize: 36)),
+              height: 100,
+              decoration: BoxDecoration(
+                  border: Border.all(color: ColorPattren.darkPurple, width: 5),
+                  borderRadius: BorderRadius.circular(20),
+                  color: ColorPattren.orangeAccent)),
+        ),
+      ),
+      backgroundColor: ColorPattren.backGroundColor,
+      body: SafeArea(
+        child: Directionality(
+          textDirection: TextDirection.rtl,
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Container(
+                decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(50)),
+                    color: ColorPattren.darkPurple),
+                width: 150,
+                child: Text(
+                  "  اضافة رحلة :",
+                  style: TextStyle(
+                    color: ColorPattren.backGroundColor,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20),
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: ColorPattren.lightPink),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text("اختر خط",
+                        style: TextStyle(
+                            fontSize: 22, fontWeight: FontWeight.bold)),
+                    DropdownButton(
+                      elevation: 0,
+                      icon: const Icon(Icons.arrow_downward_outlined),
+                      hint: const Text(''),
+                      items: ['مزة', 'جديدة', "باب توما"]
+                          .map((e) => DropdownMenuItem(
+                                value: e,
+                                child: Text(e,
+                                    style: const TextStyle(fontSize: 20)),
+                              ))
+                          .toList(),
+                      onChanged: (value) {
+                        setState(() {
+                          cityVal = value!;
+                        });
+                      },
+                      value: cityVal,
+                    ),
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.add_circle_outline,
+                          size: 35,
+                        ))
+                  ],
+                ),
+              ),
+            ),
+            MultiSelectItem(
+              height: 50,
+              buttColor: ColorPattren.lightPink,
+              listOfItem: ['mazzeh', 'asdasdsa', 'asdsadsad', 'dasdsadjhjhg'],
+              child: const Text('اختر المواقف ',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: ColorPattren.lightPink),
+                padding: EdgeInsets.all(20),
+                width: double.maxFinite,
+                child: Column(
+                  children: [
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            'اختر ساعة ويوم الانطلاق:',
+                            style: TextStyle(
+                                fontSize: 22, fontWeight: FontWeight.bold),
+                          ),
+                          IconButton(
+                              onPressed: () async {
+                                DateTime? newDate = await showDatePicker(
+                                    context: context,
+                                    initialDate: flightDate,
+                                    firstDate: DateTime(DateTime.now().year),
+                                    lastDate: DateTime(flightDate.year + 3));
+                                if (newDate == null) return;
+
+                                TimeOfDay? newTime = await showTimePicker(
+                                    context: context,
+                                    initialTime: TimeOfDay(
+                                        hour: flightDate.hour,
+                                        minute: flightDate.minute));
+                                if (newTime == null) return;
+                                setState(() {
+                                  flightDate = DateTime(
+                                      newDate.year,
+                                      newDate.month,
+                                      newDate.day,
+                                      newTime.hour,
+                                      newTime.minute);
+                                });
+                              },
+                              icon: Icon(Icons.date_range)),
+                        ]),
+                    Text(
+                      "${flightDate.year}/${flightDate.month}/${flightDate.day}  ${flightDate.hour}:${flightDate.minute}",
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 26),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: ColorPattren.lightPink),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text("اختر باص",
+                        style: TextStyle(
+                            fontSize: 22, fontWeight: FontWeight.bold)),
+                    DropdownButton(
+                      elevation: 0,
+                      icon: const Icon(Icons.arrow_downward_outlined),
+                      hint: const Text(''),
+                      items: ['1', '2', "3"]
+                          .map((e) => DropdownMenuItem(
+                                value: e,
+                                child: Text(e,
+                                    style: const TextStyle(fontSize: 20)),
+                              ))
+                          .toList(),
+                      onChanged: (value) {
+                        setState(() {
+                          busnum = value!;
+                        });
+                      },
+                      value: busnum,
+                    ),
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.add_circle_outline,
+                          size: 35,
+                        ))
+                  ],
+                ),
+              ),
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/admin_panel/widgets/multi_item.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+import '../../ui/style/app_color.dart';
+
+
+class MultiSelectItem extends StatefulWidget {
+  MultiSelectItem(
+      {
+        required this.listOfItem,
+      this.width,
+      this.height = 70,
+      this.shape,
+      required this.child,
+      this.buttColor,
+      Key? key})
+      : super(key: key);
+  Color? buttColor;
+  Widget child;
+  ShapeBorder? shape;
+  double height;
+  double? width;
+  List<String> listOfItem;
+  @override
+  State<MultiSelectItem> createState() => _MultiSelectItemState();
+}
+
+class _MultiSelectItemState extends State<MultiSelectItem> {
+  List<String> _selectedItems = [];
+  void _showMultiSelect() async {
+    final List<String> items = widget.listOfItem;
+    final List<String>? results = await showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return MultiSelect(
+            items: items,
+          );
+        });
+    if (results != null) {
+      setState(() {
+        _selectedItems = results;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0,vertical: 20),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            MaterialButton(
+              onPressed: _showMultiSelect,
+              color: widget.buttColor,
+              height: widget.height,
+              shape: widget.shape,
+                minWidth: widget.width,
+              child: widget.child,
+            ),
+            const Divider(height: 30,),
+            Wrap(
+              children: _selectedItems.map((e) => Container(child:  Text(e,style: TextStyle(color: ColorPattren.lightPink)),decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: ColorPattren.darkBlue),padding: const EdgeInsets.all(20),)).toList(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MultiSelect extends StatefulWidget {
+  MultiSelect({required this.items, Key? key}) : super(key: key);
+  final List<String> items;
+  @override
+  State<MultiSelect> createState() => _MultiSelectState();
+}
+
+class _MultiSelectState extends State<MultiSelect> {
+  final List<String> _selectedItems = [];
+
+  void _itemChange(String itemValue, bool isSelected) {
+    setState(() {
+      if (isSelected) {
+        _selectedItems.add(itemValue);
+      } else {
+        _selectedItems.remove(itemValue);
+      }
+    });
+  }
+
+  void _cancel() {
+    Navigator.pop(context);
+  }
+
+  void _submit() {
+    print(
+      _selectedItems
+    );
+    Navigator.pop(context, _selectedItems);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+        title: Directionality(textDirection: TextDirection.rtl,child: const Text('اختار المواقف:')),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: widget.items
+                .map((item) => CheckboxListTile(
+                    value: _selectedItems.contains(item),
+                    title: Text(item),
+                    controlAffinity: ListTileControlAffinity.leading,
+                    onChanged: (isChecked) => _itemChange(item, isChecked!)))
+                .toList(),
+
+          ),
+        ),
+      actions: [
+        TextButton(onPressed: _cancel, child: Text('Cancel')),
+        ElevatedButton(style: ButtonStyle(backgroundColor: MaterialStatePropertyAll(ColorPattren.lightPink,)),onPressed: _submit, child: Text('Submit'))
+      ],
+    );
+  }
+}
